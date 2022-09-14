7d28974eee9bf2875a98e05666ce13d64617e869
Voynitskiy | AlxVoy <alexey@voynitskiy.ru>
2022-09-14 15:26:21 +0300

    Update index.js

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -19,7 +19,7 @@ function processMenu() {
       title: 'blockchains',
       children: blockchains,
       tag: `${blockchains.length}`,
-      icon: 'https://ping.pub/logo.svg',
+      icon: 'https://explorer.voynitskiy.com/logo.svg',
     })
   }
   chainMenus.push({ header: 'LINKS' })
